f32ed80b6d50d88e263bc2907a827d2c51cc6b9e
esplett <estherina@gmail.com>
2018-12-03 21:43:36 +0000

    used a buffer technique to print chunks of data

--- w2d1/http-requests/getAndPrintHTML.js
+++ w2d1/http-requests/getAndPrintHTML.js
@@ -1,3 +1,5 @@
+var https = require('https');
+
 function getAndPrintHTML () {
 
   var requestOptions = {
@@ -5,6 +7,36 @@ function getAndPrintHTML () {
     path: '/http-examples/step2.html'
   };
 
-  /* Add your code here */
+   var buffer = "";
+
+    https.get(requestOptions, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      buffer = buffer + data; // buffer += data
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log(buffer);
+    });
+  });
 
 }
+
+getAndPrintHTML();
+
+
+
+
+// This function should use a buffering technique to append
+// each chunk of data to a variable as it is received, and
+// then console.log the data once all of the data has been
+// received.
+// Buffering in this context means the technique you've
+// used before (usually in loops or conditionals) of
+// creating an empty variable and adding data to it.
